f1e812294ab7013553eb4fcfac6bd35059de6d37
Marcin Kwiatkowski <m.kwiatkowski@int.pl>
2022-09-17 23:47:19 +0200

    Created redirects module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { RedirectsModule } from './modules/redirects/redirects.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [RedirectsModule],
 })
 export class AppModule {}
